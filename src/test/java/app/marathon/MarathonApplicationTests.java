638283152391bd8eee1bdc7227aa3d76b6606ea4
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2022-07-30 23:47:00 +0300

    Create `SpringBoot application`

---
+++ src/test/java/app/marathon/MarathonApplicationTests.java
@@ -0,0 +1,13 @@
+package app.marathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
